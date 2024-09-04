fef5b61050d9ab165af8d6ee4d9c0348838135dd
Geisiany <128638111+Geisiany@users.noreply.github.com>
2024-09-04 16:37:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+export const perguntas = [
+    {
+        enunciado: "como pode prejudicar o ser humano?",
+        alternativas: [
+            {
+                texto: "perda de empregos até questões de segurança e privacidade!",
+                afirmacao:[
+                 "No início ficou com medo do que essa tecnologia pode fazer. ",
+                 ""
+                ]
+            },
+            {
+                texto: "o desemprego causado pela substituição de trabalhadores por máquinas inteligentes",
+                afirmacao:[
+                 "Quis saber como usar IA no seu dia a dia.",
+                 ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Quais são os pontos positivos?",
+        alternativas: [
+            {
+                texto: "estimula a personalização, o engajamento e prepara para o futuro do trabalho",
+                afirmacao:[
+                     "Conseguiu utilizar a IA para buscar informações úteis.",
+                     ""
+                ]
+            },
+            {
+                texto: "otimizar processos de fabricação e aumentar a eficiência",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    ""
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Quais são os pontos Negativos?",
+        alternativas: [
+            {
+                texto: "Dependência Tecnológica",
+                afirmacao:[
+                     "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    ""
+                ]
+            },
+            {
+                texto: "Falta de Interação Humana",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    ""
+                ]
+            }
+        ]
+    },
+];
